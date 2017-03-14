922c2a9b2ece9131e57df2f0b0304935d4a7e5e6
sayem <sam.x092@gmail.com>
2017-03-14 14:49:54 +0600

    adding comment

--- MailTest/src/main/java/com/mail/MailTestApplication.java
+++ MailTest/src/main/java/com/mail/MailTestApplication.java
@@ -2,7 +2,7 @@ package com.mail;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//main method
 @SpringBootApplication
 public class MailTestApplication {
 
